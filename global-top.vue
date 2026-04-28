56b5f3a2fc0b608bd8ebca35bc69dbd21fe0b6ea
CastGryff <46723525+CastGryff@users.noreply.github.com>
2026-04-28 10:07:50 -0700

    Add live clock to slide deck

--- global-top.vue
+++ global-top.vue
@@ -1,1 +1,28 @@
-<template></template>
+<script setup>
+import { onMounted, onUnmounted, ref } from 'vue'
+
+const currentTime = ref('')
+let timer
+
+function updateTime() {
+  currentTime.value = new Intl.DateTimeFormat(undefined, {
+    hour: 'numeric',
+    minute: '2-digit',
+  }).format(new Date())
+}
+
+onMounted(() => {
+  updateTime()
+  timer = window.setInterval(updateTime, 1000)
+})
+
+onUnmounted(() => {
+  window.clearInterval(timer)
+})
+</script>
+
+<template>
+  <div v-if="$nav.currentPage > 1 && currentTime" class="deck-clock">
+    {{ currentTime }}
+  </div>
+</template>
